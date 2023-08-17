9fdf3ccec44cd3a60a7e0b02d2b789f314d32fb0
Minhajul Islam <wrench786@gmail.com>
2023-08-18 03:10:34 +0600

    up

--- Test it 1.cpp
+++ Test it 1.cpp
@@ -9,32 +9,7 @@ using namespace std;
 
 // check int overflow4
 void solve(){
-    string str;
-    cin>>str;
-
-    int n = str.size();
-
-    string a = string(n,'(') + string(n,')');
-    string b ="";
-
-    for(int i=1;i<=n;i++){
-        b+='(';
-        b+=')';
-    }
-    //hi
-
-    if(str=="()") cout<<"NO"<<nn;
-    else{
-        cout<<"YES"<<nn;
-
-        int flag=0;
-        for(int i=0;i<n-1;i++){
-            if(str[i]==str[i+1]) flag=1;
-        }
-
-        if(flag)cout<<b<<nn;
-        else cout<<a<<nn;
-    }
+    cout << "w" << endl;
 }
  
 int main()
