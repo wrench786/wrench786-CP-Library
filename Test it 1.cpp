1a8f964a0075b7b59dbf7f0cda35409be365841c
Minhajul Islam <wrench786@gmail.com>
2023-08-31 20:31:03 +0600

    update

--- Test it 1.cpp
+++ Test it 1.cpp
@@ -5,22 +5,25 @@ typedef unsigned long long ull;
 #define mod 1000000007
 #define mod2 998244353
 
+const ll N = 1e6+7;
+
 using namespace std;
 
 // check int overflow
 void solve(){
 
 }
+
  
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
- 
+
     // freopen("reduce.in", "r", stdin);
     // freopen("reduce.out", "w", stdout);
     int tc=1;
-    //cin>>tc;
+    cin>>tc;
     //scanf("%d",&tc);  
     while(tc--){
         solve();
