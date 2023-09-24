fa2a52f6533195c7e3846b2a5cff5b2c5bc8eaef
Md. Minhajul Islam <wrench786@gmail.com>
2023-09-24 19:29:49 +0600

    update

--- Need More Algo/Segment Tree.cpp
+++ Need More Algo/Segment Tree.cpp
@@ -78,13 +78,17 @@ void solve(){
 
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
     // freopen("reduce.in", "r", stdin);
     // freopen("reduce.out", "w", stdout);
     int tc=1;
     //cin>>tc;
-    //scanf("%d",&tc);  
+    
+    //int cases=0;
     while(tc--){
+        //cout<<"Case "<<++cases<<":"<<nn;
         solve();
     }
     return 0;
--- Test it 1.cpp
+++ Test it 1.cpp
@@ -1,20 +1,19 @@
 #include<bits/stdc++.h>
+using namespace std;
+
 typedef long long ll;
 typedef unsigned long long ull;
 #define nn "\n"
 #define mod 1000000007
-using namespace std;
 
 #define mx 1000000
 
-int tree[mx*4];
-int min_count[mx*4];
+ll tree[mx*4];
 int arr[mx];
 
 void tree_build(int node, int ls, int rs){
     if(ls==rs){
         tree[node] = arr[ls];
-        min_count[node] = 1;
         return;
     }
     int left = node*2;
@@ -23,23 +22,17 @@ void tree_build(int node, int ls, int rs){
 
     tree_build(left,ls,mid);
     tree_build(right,mid+1,rs);
-
-    tree[node] = min( tree[left], tree[right]); // change
     
-    if(tree[left]<tree[right]){
-        min_count[node] = min_count[left];
-    }
-    else if(tree[left]>tree[right]){
-        min_count[node] = min_count[right];
-    }
-    else min_count[node] = min_count[left]+min_count[right];
-    
+    tree[node] = tree[left]+tree[right]; // change
 }
 
 void tree_update(int node, int ls, int rs, int idx, int value){
     if( idx<ls || rs<idx ) return;
     if( ls==rs && ls==idx ){
-        tree[node] = value;
+        tree[node] += 1ll*value;
+
+        if(value==0) tree[node] = 0;
+        
         return;
     }
     int left = node*2;
@@ -49,78 +42,49 @@ void tree_update(int node, int ls, int rs, int idx, int value){
     tree_update(left,ls,mid,idx,value);
     tree_update(right,mid+1,rs,idx,value);
     
-    tree[node] = min( tree[left], tree[right]); // change
-
-    if(tree[left]<tree[right]){
-        min_count[node] = min_count[left];
-    }
-    else if(tree[left]>tree[right]){
-        min_count[node] = min_count[right];
-    }
-    else min_count[node] = min_count[left]+min_count[right];
+    tree[node] = tree[left]+tree[right]; //change
 }
 
 
-pair<int,int> tree_query(int node, int ls, int rs, int x, int y){
-    if(ls>y || rs<x) return make_pair(INT_MAX,0); //change
-    if(ls>=x && rs<=y) return make_pair(tree[node],min_count[node]);
+ll tree_query(int node, int ls, int rs, int x, int y){
+    if(ls>y || rs<x) return 0; //change
+    if(ls>=x && rs<=y) return tree[node];
     
     int left = node*2;
     int right = node*2+1;
     int mid = (ls+rs)/2;
 
-    pair<int,int>res1 = tree_query(left,ls, mid, x, y);
-    pair<int,int>res2 = tree_query(right, mid+1, rs, x, y);
+    ll res1 = tree_query(left,ls, mid, x, y);
+    ll res2 = tree_query(right, mid+1, rs, x, y);
     
-    int mn = min(res1.first,res2.first);
-    int cnt=0;
-
-    if(mn==res1.first) cnt+= res1.second;
-    if(mn==res2.first) cnt+= res2.second;
-
-    return make_pair(mn,cnt);
+    return res1+res2; //change
 }
 
 // check int overflow
 void solve(){
-    int n,q;
-    cin>>n>>q;
+    int n;
+    cin>>n;
 
     for(int i=1;i<=n;i++) cin>>arr[i];
 
     tree_build(1,1,n);
 
-    while(q--){
-        int type;
-        cin>>type;
-
-        if(type==1){
-            int idx, value;
-            cin>>idx>>value;
-
-            tree_update(1,1,n,idx+1,value);
-        }   
-        else{
-            int l,r;
-            cin>>l>>r;
-
-            pair<int,int>ans =  tree_query(1,1,n,l+1,r);
-
-            cout<<ans.first<<" "<<ans.second<<nn;
-
-        }
-    }
+    
 }   
 
 int main()
 {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
     // freopen("reduce.in", "r", stdin);
     // freopen("reduce.out", "w", stdout);
     int tc=1;
     //cin>>tc;
-    //scanf("%d",&tc);  
+    
+    //int cases=0;
     while(tc--){
+        //cout<<"Case "<<++cases<<":"<<nn;
         solve();
     }
     return 0;
